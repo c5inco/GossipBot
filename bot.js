0a1555e0132bb98ad948e4aa6ad99d026d493031
Chris Sinco <csinco@microsoft.com>
2016-02-04 23:47:13 -0800

    Some interesting multi-turn

--- bot.js
+++ bot.js
@@ -15,7 +15,6 @@ var bot = controller.spawn({
     token: config.token
 }).startRTM();
 
-
 controller.hears(['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
     bot.api.reactions.add({
         timestamp: message.ts,
@@ -34,4 +33,68 @@ controller.hears(['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'
             bot.reply(message,'Hello.');
         }
     });
+});
+
+
+var endCallback =  function(response, convo) {
+    convo.say('Alrighty then, ciao!');
+    convo.next();
+};
+
+var askForLatest = function(response, convo) {
+    convo.ask('What\'s the latest?', [
+        {
+            pattern: ['^(nevermind|got nothing|nvm|nada)'],
+            callback: function(response, convo) {
+                endCallback(response, convo);
+                convo.next();
+            }
+        },
+        {
+            default: true,
+            callback: function(response, convo) {
+                console.log(response.text);
+                askGotMore(response, convo);
+                convo.next();
+            }
+        }
+    ]);
+};
+
+var askGotMore = function(response, convo) {
+    convo.ask('Interesting... Got more?', [
+        {
+            pattern: bot.utterances.no,
+            callback: function(response, convo) {
+                endCallback(response, convo);
+                convo.next();
+            }
+        },
+        {
+            pattern: bot.utterances.yes,
+            callback: function(response, convo) {
+                askForLatest(response, convo);
+                convo.next();
+            }
+        },
+        {
+            default: true,
+            callback: function(response, convo) {
+                convo.repeat();
+                convo.next();
+            }
+        }
+    ]);
+}
+
+var inputUtterances = [
+    '^want to hear something juicy\\?*',
+    '^i (got|have) a secret',
+    '^i heard a rumor'
+];
+
+controller.hears(inputUtterances, ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+    bot.reply(message, 'Do tell - let\'s chat privately.');
+    
+    bot.startPrivateConversation(message, askForLatest);
 });
